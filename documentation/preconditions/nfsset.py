caae33f373bb8b12d6e1261d4561864f14091d49
noadmin <2011.development@gmial.com>
2015-01-29 06:49:05 -0800

    Day 1,5

--- documentation/preconditions/nfsset.py
+++ documentation/preconditions/nfsset.py
@@ -1,3 +1,5 @@
+from getpass import _raw_input
+
 __author__ = 'user'
 #geting system access
 import subprocess
@@ -6,13 +8,25 @@ import sys
 
 #checking super access
 euid = os.geteuid()
-if euid != 0:'''
+if euid != 0:
     print ("There is no root access. Try sudo...")
     args = ['sudo', sys.executable] + sys.argv + [os.environ]
     # Trying to rerun this script (following process) through sudo.
-    os.execlpe('sudo', *args)'''
-    os.popen('echo 12345678 | sudo -s')
+    os.execlpe('sudo', *args)
 print ('Root access granted - ', euid)
-#get super access
+#using super access
+#adding required packages
 
-subprocess.call('apt-get update', shell=True)
+answer = raw_input("Owerwrite log yes or no? Default No. ").lower()
+print('answer value is ', answer)
+if answer == '':
+    answer = "no"
+    print('answer value is ',answer)
+if answer == "yes" or answer == "y":
+    subprocess.call('apt-get update -y > echo nfsset.log', shell=True)
+    subprocess.call('apt-get install nfs-kernel-server -y >> echo nfsset.log', shell=True)
+elif answer == "no" or answer == "n":
+    subprocess.call('apt-get update -y >> echo nfsset.log', shell=True)
+    subprocess.call('apt-get install nfs-kernel-server -y >> echo nfsset.log', shell=True)
+
+
