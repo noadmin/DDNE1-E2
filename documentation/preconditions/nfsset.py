7188d162ff15d003e80468bbfaedef300a8b7387
noadmin <2011.development@gmial.com>
2015-01-30 05:00:28 -0800

    Day 2

--- documentation/preconditions/nfsset.py
+++ documentation/preconditions/nfsset.py
@@ -16,17 +16,27 @@ if euid != 0:
 print ('Root access granted - ', euid)
 #using super access
 #adding required packages
-
+__path = (os.path.abspath(__file__).replace(os.path.basename(__file__), ''))
 answer = raw_input("Owerwrite log yes or no? Default No. ").lower()
 print('answer value is ', answer)
 if answer == '':
     answer = "no"
     print('answer value is ',answer)
+    print('file is ',__file__)
+    print('name is ',__name__)
+    print('basename is ', os.path.basename(__file__))
+    print(os.path.dirname(__file__)+'/')
+    print('voodoo',(os.path.abspath(__file__).replace(os.path.basename(__file__), '') + 'nfsset.log'))
 if answer == "yes" or answer == "y":
-    subprocess.call('apt-get update -y > echo nfsset.log', shell=True)
-    subprocess.call('apt-get install nfs-kernel-server -y >> echo nfsset.log', shell=True)
+    subprocess.call('date ', '| ', 'tee nfsset.log', shell=True)
+    """subprocess.call('apt-get update -y | tee -a nfsset.log', shell=True)
+    subprocess.call('apt-get install nfs-kernel-server -y | tee -a nfsset.log', shell=True)
+    #subprocess.call('mv -f nfsset.log ../../logs/', shell=True)"""
 elif answer == "no" or answer == "n":
-    subprocess.call('apt-get update -y >> echo nfsset.log', shell=True)
-    subprocess.call('apt-get install nfs-kernel-server -y >> echo nfsset.log', shell=True)
+    subprocess.call('date | tee -a %s' % (os.path.abspath(__file__).replace(os.path.basename(__file__), '') + '/nfsset.log'), shell=True)
+    """subprocess.call('apt-get update -y | tee -a nfsset.log', shell=True)
+    subprocess.call('apt-get install nfs-kernel-server -y | tee -a nfsset.log', shell=True)
+    #subprocess.call('mv -i nfsset.log ../../logs/', shell=True)"""
+
 
 
